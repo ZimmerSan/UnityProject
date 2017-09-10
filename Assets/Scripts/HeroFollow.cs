8ec166a82726467af31b81d681389827a532a708
zimmersan <paultsimura@gmail.com>
2017-09-10 22:57:14 +0300

    Basic Rabbit physics and sprite

---
+++ Assets/Scripts/HeroFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroRabbit rabbit;
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform rabbitTransform = rabbit.transform;
+		Transform cameraTransform = this.transform;
+
+		Vector3 rabbitPosition = rabbitTransform.position;
+		Vector3 cameraPosition = cameraTransform.position;
+
+		cameraPosition.x = rabbitPosition.x;
+		cameraPosition.y = rabbitPosition.y;
+
+		cameraTransform.position = cameraPosition;
+	}
+}
---
+++ Assets/Scripts/HeroRabbit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabbit : MonoBehaviour {
+
+	public float speed = 1;
+	SpriteRenderer spriteRenderer = null;
+	Rigidbody2D myBody = null;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		myBody = this.GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		float value = Input.GetAxis ("Horizontal");
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+
+			if(value < 0) {
+				spriteRenderer.flipX = true;
+			} else if(value > 0) {
+				spriteRenderer.flipX = false;
+			}
+		}
+	}
+
+}
